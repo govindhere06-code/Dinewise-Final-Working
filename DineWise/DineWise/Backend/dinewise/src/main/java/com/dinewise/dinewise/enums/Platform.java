111ee3644437742d264c2c5c2fd6ae5d99a3ae32
govindhere06-code <govindhere.06@gmail.com>
2026-05-04 11:27:32 +0530

    Deleted Folder

--- DineWise/DineWise/Backend/dinewise/src/main/java/com/dinewise/dinewise/enums/Platform.java
+++
@@ -1,7 +0,0 @@
-package com.dinewise.dinewise.enums;
-
-public enum Platform {
-    GOOGLE,
-    ZOMATO,
-    SWIGGY
-}
--- DineWise/DineWise/Backend/dinewise/src/main/java/com/dinewise/dinewise/repository/PlatformRatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dinewise.dinewise.repository;
-
-import com.dinewise.dinewise.entity.PlatformRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlatformRatingRepository extends JpaRepository<PlatformRating, Long> {
-}
